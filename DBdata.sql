9c0d2686c23849d6b826bd51864af148bd1fd7c3
edwinzhu02 <42624908+edwinzhu02@users.noreply.github.com>
2019-01-27 19:01:28 +1300

    DBData.sql

---
+++ DBdata.sql
@@ -0,0 +1,8 @@
+insert into ProductCategory values('chairs');
+insert into ProductCategory values('tables');
+insert into ProductCategory values('letters with light');
+
+
+insert into ProductType values('clown service');
+insert into ProductType values('wedding package');
+insert into ProductType values('product rental');
